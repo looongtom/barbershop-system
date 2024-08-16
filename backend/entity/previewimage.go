ffeb9d13019a15063176872c56d7dbf77bce3d35
Minh Tuan <looongtommy27@gmail.com>
2024-08-16 13:31:15 +0700

    add preview image svc

---
+++ backend/entity/previewimage.go
@@ -0,0 +1,8 @@
+package entity
+
+type PreviewImages struct {
+	ID        int    `json:"id,omitempty"`
+	Url       string `json:"url"`
+	CreatedAt int64  `json:"created_at,omitempty"`
+	AccountId int    `json:"account_id"`
+}
